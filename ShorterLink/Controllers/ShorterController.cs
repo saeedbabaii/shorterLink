3efeb2130f8d4d857918f146985c63cb2945d303
saeedbabaei <saeedbabaei1995@yahoo.com>
2020-04-23 09:53:29 +0430

    complete structure

---
+++ ShorterLink/Controllers/ShorterController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShorterLink.Repositories;
+namespace ShorterLink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShorterController : ControllerBase
+    {
+        private readonly ILinkRepository _linkRepository;
+        public ShorterController(ILinkRepository linkRepository)
+        {
+            _linkRepository = linkRepository;
+        }
+        // GET: api/Shorter
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Shorter/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Shorter
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Shorter/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ShorterLink/Models/LinkDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShorterLink.Models
+{
+    public class LinkDbContext : IdentityDbContext
+    {
+        public LinkDbContext(DbContextOptions<LinkDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Link> Pies { get; set; }
+    }
+}
